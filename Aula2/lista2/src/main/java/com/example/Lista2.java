c541c43d889ed22ac2d006fbc681cbedc8b38c2c
PhDahmer <pedrohdahmer@proton.me>
2026-02-26 19:48:26 -0300

    feat: Lista I - começo

---
+++ Aula2/lista2/src/main/java/com/example/Lista2.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Lista2 {
+    public static void main(String[] args) {
+        int numero = 1; 
+        for (int i = 0; numero <= 100; i ++) {
+            System.out.println("nuemero: " + numero);
+            numero+=2;
+        }
+    }
+}
